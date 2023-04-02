d44f9574295003512fb8fdff59e96fb48dc59972
otcz <carrillozuletaoscar@gmail.com>
2023-04-02 15:47:16 -0500

    Comit 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module mil.escudo {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
 
     opens mil.escudo to javafx.fxml;
     exports mil.escudo;
