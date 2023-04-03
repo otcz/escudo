0b543ea4ed45a918d3374436ccc67091afa01beb
otcz <carrillozuletaoscar@gmail.com>
2023-04-02 20:47:38 -0500

    correccion

--- src/main/java/mil/escudo/Controller.java
+++ src/main/java/mil/escudo/Controller.java
@@ -22,31 +22,39 @@ public class Controller {
     public void initialize() {
         try {
             // Carga la biblioteca OpenCV
+            System.out.println("1");
             System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
+            System.out.println("2");
             // Crea el objeto VideoCapture para acceder a la cámara
             capture = new VideoCapture(0);
-
+            System.out.println("3");
             // Si la cámara no está disponible, detiene la aplicación
             if (!capture.isOpened()) {
+                System.out.println("4");
                 System.err.println("Error: No se pudo acceder a la cámara.");
                 System.exit(1);
+                System.out.println("5");
             }
-
+            System.out.println("6");
             // Crea un hilo para leer cuadros de video de la cámara y mostrarlos en el ImageView
             new Thread(() -> {
+                System.out.println("7");
                 while (true) {
+                    System.out.println("8");
                     // Lee un cuadro de video de la cámara
                     Mat frame = new Mat();
+                    System.out.println("9");
                     capture.read(frame);
-
+                    System.out.println("10");
                     // Convierte la matriz de OpenCV a una imagen de JavaFX
                     Image image = mat2Image(frame);
-
+                    System.out.println("11");
                     // Actualiza el ImageView con la imagen
                     imageView.setImage(image);
+                    System.out.println("12");
                 }
             }).start();
+            System.out.println("13");
         } catch (Exception e) {
             System.out.println("ERROR");
         }
