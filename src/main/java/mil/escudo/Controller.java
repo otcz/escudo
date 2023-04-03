89f21d19cb341027c03bc74ae955866964371b24
otcz <carrillozuletaoscar@gmail.com>
2023-04-02 20:22:19 -0500

    pASO 1

--- src/main/java/mil/escudo/Controller.java
+++ src/main/java/mil/escudo/Controller.java
@@ -3,7 +3,7 @@ package mil.escudo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
